c757f1e4d33d47eea3d95c8e0e66093a2a272f68
jatinkaushik-jk <jkprogrammer25@gmail.com>
2024-10-14 07:22:12 +0530

    smooth scroll

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 import "./src/threejsScript"
 import "./src/aiChat"
+import "./src/locomotive-scroll.min.js"
 // import "./src/particles"
 // import "./src/cards.js"
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true,
+});
+
 
 const menuToggle = document.getElementById('menu-toggle');
 const mobileMenu = document.getElementById('mobile-menu');
